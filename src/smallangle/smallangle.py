62908b91cb5893effb839841244f8480e235876e
Samcoder23 <139370735+Samcoder23@users.noreply.github.com>
2023-11-17 16:18:59 +0100

    Subcommands sin and tan were made and the option was made to give an endvalue for the function.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,22 +1,61 @@
+# Author: Sameh Mikhail
+# Student number: 14003597
+# Date: 17-11-2023
+
 import click
 import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+@cmd_group.command()
+@click.option(
+    "-v",
+    "--value",
+    default=1,
+    help="Argument for chosen function.",
+    show_default=True,  # show default in help
+)
+def sin(value):
+    """Function to give a sine of the x value
+
+    Args:
+        number (int): value to give the end of the 
+
+    Returns:
+        list: containing the integers
+    """
+    x = np.linspace(0, 2 * pi, value)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
 
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+@cmd_group.command()
+@click.option(
+    "-v",
+    "--value",
+    default=1,
+    help="Argument for chosen function.",
+    show_default=True,  # show default in help
+)
+def tan(value):
+    """List integers up to a given number.
+
+    Args:
+        number (int): list integers up to this number
+
+    Returns:
+        list: containing the integers
+    """
+    x = np.linspace(0, 2 * pi, value)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
 
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
